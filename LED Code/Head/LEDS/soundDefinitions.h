938663be8ba35f3bf8643b1c22a4d1427ed0762c
tobenamed <bhargavjoshi855@gmail.com>
2017-05-27 23:25:56 -0400

    Update soundDefinitions.h

--- LED Code/Head/LEDS/soundDefinitions.h
+++ LED Code/Head/LEDS/soundDefinitions.h
@@ -1,6 +1,9 @@
 #ifndef soundDefinitions_h
 #define soundDefinitions_h
 
+#include "WaveHC.h"
+#include "waveUtil.h"
+
 int button1 = 1;
 int button2 = 2;
 int button3 = 3;
@@ -11,5 +14,8 @@ int button2state = 0;
 int button3state = 0;
 int button4state = 0;
 
+void sndBOOP()
+playcomplete("103.WAV")
 
+  
 #endif
