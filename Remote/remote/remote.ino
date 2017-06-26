a919f776f0d1067a5d0a147824c03b5f78598f53
Michael Wilke <enarchtechnologies@gmail.com>
2017-06-26 17:42:11 -0500

    Formatting Fix

--- Remote/remote/remote.ino
+++ Remote/remote/remote.ino
@@ -1,69 +1,67 @@
-int PinNullBtn = 0 ;
-int PinOneBtn = 1 ;
-int PinTwoBtn = 2 ;
-int PinThreeBtn = 3 ;
-int PinFourBtn = 4 ;
-int PinFiveBtn = 5 ;
-int PinSixBtn = 6 ;
-int PinSevenBtn = 7 ;
-int PinEightBtn = 8 ;
-int PinNineBtn = 9 ;
+int PinNullBtn = 0;
+int PinOneBtn = 1;
+int PinTwoBtn = 2;
+int PinThreeBtn = 3;
+int PinFourBtn = 4;
+int PinFiveBtn = 5;
+int PinSixBtn = 6;
+int PinSevenBtn = 7;
+int PinEightBtn = 8;
+int PinNineBtn = 9;
 
 void setup() {
-  Serial.begin(9600) ;
-pinMode(PinNullBtn, INPUT) ;
-pinMode(PinTwoBtn, INPUT) ;
-pinMode(PinOneBtn, INPUT) ;
-pinMode(PinThreeBtn, INPUT) ;
-pinMode(PinFourBtn, INPUT) ;
-pinMode(PinFiveBtn, INPUT) ;
-pinMode(PinSixBtn, INPUT) ;
-pinMode(PinSevenBtn, INPUT) ;
-pinMode(PinEightBtn, INPUT) ;
-pinMode(PinNineBtn, INPUT) ;
+  Serial.begin(9600);
+  pinMode(PinNullBtn, INPUT);
+  pinMode(PinTwoBtn, INPUT);
+  pinMode(PinOneBtn, INPUT);
+  pinMode(PinThreeBtn, INPUT);
+  pinMode(PinFourBtn, INPUT);
+  pinMode(PinFiveBtn, INPUT);
+  pinMode(PinSixBtn, INPUT);
+  pinMode(PinSevenBtn, INPUT);
+  pinMode(PinEightBtn, INPUT);
+  pinMode(PinNineBtn, INPUT);
 }
 
 void loop() {
   
-if ( digitalRead(PinNullBtn)= "HIGH" )
-{
-  Serial.println("a") ; 
-} 
-else if (   digitalRead(PinOneBtn) = "HIGH" )
-{ 
-  Serial.println("b") ;
-}
-else if (  digitalRead(PinTwoBtn) = "HIGH" )
-{ 
-  Serial.println("c") ;
-}
-else if (   digitalRead(PinThreeBtn) = "HIGH" )
-{ 
-  Serial.println("d") ;
-}
-else if (  digitalRead(PinFourBtn)
- = "HIGH" )
-{ 
-  Serial.println("e") ;
-}
-else if ( digitalRead(PinFiveBtn = "HIGH" )
-{ 
-  Serial.println("f") ;
-}
-else if ( digitalRead(PinSixBtn = "HIGH" )
-{ 
-  Serial.println("g") ;
-}
-else if ( PinSevenBtn = "HIGH" )
-{ 
-  Serial.println("h") ;
-}
-else if ( PinEightBtn = "HIGH" )
-{ 
-  Serial.println("i") ;
-}
-else if ( PinNineBtn = "HIGH" )
-{ 
-  Serial.println("j") ;
-}
+  if (digitalRead(PinNullBtn)= "HIGH") {
+    Serial.println("a"); 
+  } 
+ 
+  else if (digitalRead(PinOneBtn) = "HIGH") { 
+    Serial.println("b");
+  }
+
+  else if (digitalRead(PinTwoBtn) = "HIGH") { 
+    Serial.println("c");
+  }
+ 
+  else if (digitalRead(PinThreeBtn) = "HIGH") { 
+    Serial.println("d");
+  }
+
+  else if (digitalRead(PinFourBtn) = "HIGH") { 
+    Serial.println("e");
+  }
+
+  else if ( digitalRead(PinFiveBtn = "HIGH" ) { 
+    Serial.println("f");
+  }
+
+  else if ( digitalRead(PinSixBtn = "HIGH" ) { 
+    Serial.println("g");
+  }
+
+  else if ( PinSevenBtn = "HIGH") { 
+    Serial.println("h");
+  }
+           
+  else if (PinEightBtn = "HIGH") { 
+    Serial.println("i") ;
+  }
+
+  else if (PinNineBtn = "HIGH") { 
+    Serial.println("j") ;
+  }
 }
