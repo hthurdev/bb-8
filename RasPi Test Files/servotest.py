50e903e02666e62c72531d6311b638793228d848
hthurdev <hthurmank2c@outlook.com>
2025-09-18 22:04:49 -0500

    Added head servo test cycle and renamed vars to follow convention.

--- RasPi Test Files/servotest.py
+++ RasPi Test Files/servotest.py
@@ -17,16 +17,16 @@ pca.frequency = 50  # 50Hz for servos
 
 ### Testing Control Area ###
 #enable ONLY ONE boolean for what you want to test
-DCdrivemotorsTest=False
-SwingServoTest=False
-HeadServoTest=True
+DC_drive_motors_test=False
+swing_servo_test=False
+head_servo_test=True
 RestAllServos=False
 ############################
 
 ### DC Motors and Driver Relay Definitions ###
-DCmotor1 = PWMOutputDevice(13, initial_value=0.5, frequency=10000)
-DCmotor2 = PWMOutputDevice(18, initial_value=0.5, frequency=10000)
-motorDriverRelay = OutputDevice(17, active_high=False)
+DC_motor1 = PWMOutputDevice(13, initial_value=0.5, frequency=10000)
+DC_motor2 = PWMOutputDevice(18, initial_value=0.5, frequency=10000)
+motor_driver_relay = OutputDevice(17, active_high=False)
 ##################################
 
 ### Gyro Definitions ###
@@ -35,12 +35,12 @@ last_val = 0xFFFF
 ########################
 
 ### Servo Definitions ###
-#!!USE setSwing!! instead of setting the angle of individual swingServo motors#
-swingServo = servo.Servo(pca.channels[0]) #use setSwing to not break the opposite servo!!
-headFB = servo.Servo(pca.channels[2]) #front to back CENTER: 105
-headS2S = servo.Servo(pca.channels[1]) #side to side CENTER: 95
-headRotate = servo.ContinuousServo(pca.channels[3], min_pulse=810, max_pulse=2300) #self explanatory
-swingServo2 = servo.Servo(pca.channels[4]) #other swing servo. It swings opposite to the first one. USE SETSWING
+#!!USE set_swing!! instead of setting the angle of individual swing_servo motors#
+swing_servo = servo.Servo(pca.channels[0]) #use set_swing to not break the opposite servo!!
+head_fb = servo.Servo(pca.channels[2]) #front to back CENTER: 105
+head_sts = servo.Servo(pca.channels[1]) #side to side CENTER: 95
+head_rotate = servo.ContinuousServo(pca.channels[3], min_pulse=810, max_pulse=2300) #self explanatory
+swing_servo2 = servo.Servo(pca.channels[4]) #other swing servo. It swings opposite to the first one. USE set_swing
 #########################
 
 
@@ -49,10 +49,10 @@ swingServo2 = servo.Servo(pca.channels[4]) #other swing servo. It swings opposit
 print("TO CANCEL, USE CTRL+C, DONT CLOSE THE TAB")
 print("Waiting 2 secs")
 time.sleep(2)
-motorDriverRelay.on()
+motor_driver_relay.on()
 
 ### quality of life servo functions ###
-def easyTurn(servo, angle, timeperdegree):
+def easy_turn(servo, angle, timeperdegree):
 	#This function allows to fine tune the turn rate of a servo
 	if angle>int(servo.angle):
 		for angle in range(int(servo.angle),angle,1):
@@ -63,30 +63,30 @@ def easyTurn(servo, angle, timeperdegree):
 			servo.angle=angle
 			time.sleep(timeperdegree)
 			
-def setSwing(degrees): #rolling straight is 90
+def set_swing(degrees): #rolling straight is 90
 	#max 125 
 	#min 70
 	if 125>=degrees>=70:
 		difference=degrees-90
-		swingServo.angle = 90 + difference
-		swingServo2.angle = 90 - difference
+		swing_servo.angle = 90 + difference
+		swing_servo2.angle = 90 - difference
 	else:
 		print("swing out of range: max 115 minimum 70")
 #used for testing to "disable" servos who may be retaining their previous position
 
-def setHeadFB(degrees):
+def set_head_fb(degrees):
 	#center: 105
-	if 65<degrees+15<150:
-		headFB.angle = degrees+15;
+	if 65<=degrees+15<=150:
+		head_fb.angle = degrees+15;
 	else:
-		print("headFB can't go out of range")
+		print("head_fb can't go out of range")
 
-def setHeadS2S(degrees):
+def set_head_sts(degrees):
 	#center: 95
-	if 45<degrees+5<145:
-		headS2S.angle = degrees+5;
+	if 45<=degrees+5<=145:
+		head_sts.angle = degrees+5;
 	else:
-		print("headS2S can't go out of range")
+		print("head_sts can't go out of range")
 
 
 servo_floppy = [False] * 16  # up to 16 channels
@@ -120,18 +120,18 @@ def set_servo_angle(channel, angle):
         print(f"Channel {channel} is in floppy mode; ignoring command.")
 #######################################	
 
-def headTurn(degrees):
+def head_turn(degrees):
 	#2.65 seconds per turn at speed 1     22.64rpm
 	#2.65 seconds per turn at speed -0.92 22.64rpm
-	turnseconds=abs(degrees*(2.5/360))
+	turn_seconds=abs(degrees*(2.5/360))
 	if degrees>0:
-		headRotate.throttle=1
-		time.sleep(turnseconds)
-		headRotate.throttle=0
+		head_rotate.throttle=1
+		time.sleep(turn_seconds)
+		head_rotate.throttle=0
 	if degrees<0:
-		headRotate.throttle=-0.92
-		time.sleep(turnseconds)
-		headRotate.throttle=0
+		head_rotate.throttle=-0.92
+		time.sleep(turn_seconds)
+		head_rotate.throttle=0
 	
 
 # Testing is contained in a Try except loop to turn off the DC motor driver 
@@ -144,44 +144,69 @@ def headTurn(degrees):
 # 0.5 is neutral
 while True:
 	try:
-		if DCdrivemotorsTest:
+		if DC_drive_motors_test:
 			time.sleep(2)
 			print("reverse motors")
-			DCmotor1.value = 0.6
-			DCmotor2.value = 0.6
+			DC_motor1.value = 0.6
+			DC_motor2.value = 0.6
 												
 			time.sleep(2)
 			
-			DCmotor1.value = 0.5
-			DCmotor2.value = 0.5
+			DC_motor1.value = 0.5
+			DC_motor2.value = 0.5
 			
 			time.sleep(2)
 			
-			DCmotor1.value = 0.4
-			DCmotor2.value = 0.4
+			DC_motor1.value = 0.4
+			DC_motor2.value = 0.4
 			
 			time.sleep(2)
 			
-			DCmotor1.value = 0.5
-			DCmotor2.value = 0.5
+			DC_motor1.value = 0.5
+			DC_motor2.value = 0.5
 			
-		if SwingServoTest:
+		if swing_servo_test:
 			# Quick Servo Test swings it back and forth
 
 
-			setSwing(70)
+			set_swing(70) 
 			time.sleep(2)
-			setSwing(117)
+			set_swing(117)
 			time.sleep(2)
 			
-		if HeadServoTest:
+		if head_servo_test:
 			# Test Head Servos
-			
-			setHeadFB(90)
-			setHeadS2S(90)
-			headTurn(180)
+			# Set all to neutral position
+			set_head_fb(90) #65-150
+			set_head_sts(90) #45-145
 			time.sleep(1)
-			headTurn(180)
+
+			# Range test forward-backwards
+			set_head_fb(65)
+			time.sleep(2)
+			set_head_fb(150)
+			time.sleep(2)
+			set_head_fb(90)
+			time.sleep(1)
+
+			# Range test side to side
+			set_head_sts(45)
+			time.sleep(2)
+			set_head_sts(145)
+			time.sleep(2)
+			set_head_sts(90)
+			time.sleep(1)
+
+			# Accuracy test of continuous head turning motor
+			head_turn(180)
+			time.sleep(2)
+			head_turn(-180)
+			time.sleep(2)
+			head_turn(-180)
+			time.sleep(2)
+			head_turn(180)
+			time.sleep(1)
+			head_turn(360)
 			
 			time.sleep(2)
 			
@@ -191,10 +216,10 @@ while True:
 	except KeyboardInterrupt:
 		
 		#this sets the speed of the motors to 0 and turns off the relay.
-		DCmotor2.value = 0.5
-		DCmotor1.value = 0.5
+		DC_motor2.value = 0.5
+		DC_motor1.value = 0.5
 		
-		motorDriverRelay.off()
+		motor_driver_relay.off()
 		rest_all_servos()
 		print("Exiting")
 		time.sleep(2)
