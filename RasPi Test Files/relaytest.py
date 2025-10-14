e1fe1e3876717d8674ea91ffbb3b1df59f4e709f
hthurdev <hthurmank2c@outlook.com>
2025-10-14 10:28:51 -0500

    Added relay

--- RasPi Test Files/relaytest.py
+++ RasPi Test Files/relaytest.py
@@ -4,6 +4,7 @@ import time
 
 
 relay = OutputDevice(17, active_high=False)
+turn_driver_relay = OutputDevice(27, active_high=False)
 while True:
 	
 	# This tests the relay that controls the DC motor driver. I deemed a relay 
@@ -15,9 +16,17 @@ while True:
 	
 	relay.on()
 	print("Relay on")
-	time.sleep(5)
+	time.sleep(3)
 	relay.off()
 	print("Relay off")
-	time.sleep(5)
+	time.sleep(3)
+	
+	turn_driver_relay.on()
+	print("Turn Relay on")
+	time.sleep(3)
+
+	turn_driver_relay.off()
+	print("Turn Relay off")
+	time.sleep(3)
 	
 
